284c491f01c46caf221aeab8810036b6b4dd1981
Khai <hoangdangkhai.dev@gmail.com>
2025-06-29 13:48:40 +0000

    feat: clean code, reduce redundant functions

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
 import asyncio
 import aiohttp
-from config import MONITORING_SERVICE_URL, COMPUTE_ID, INTERVAL_SECONDS
-from uploader import upload_directory_to_s3
-
 import os
-task_id = os.getenv("TASK_ID", "default_value")
+from datetime import datetime
 
+TASK_ID = os.getenv("TASK_ID", "null")
+MONITORING_SERVICE_API = os.getenv("MONITORING_SERVICE_API", "http://35.198.209.90:10303/api/heartbeat")
+INTERVAL_SECONDS = int(os.getenv("INTERVAL_SECONDS", "60"))
 
-UPLOAD_INTERVAL_SECONDS = 60
-UPLOAD_FOLDER = "test_model"
-
+if TASK_ID == "null":
+    print("[X] TASK_ID environment variable is not set. Exiting.")
+    exit(1)
 
 async def send_heartbeat(session: aiohttp.ClientSession):
     try:
         async with session.post(
-            MONITORING_SERVICE_URL, json={"task_id": task_id}
+            MONITORING_SERVICE_API, json={"task_id": TASK_ID}
         ) as response:
             if response.status == 200:
-                print(f"[✓] Heartbeat sent for {COMPUTE_ID}")
+                print(f"[{datetime.now().isoformat()}] [✓] Heartbeat sent for {TASK_ID}")
             else:
                 text = await response.text()
-                print(f"[!] Failed to send heartbeat: {response.status} - {text}")
+                print(f"[{datetime.now().isoformat()}] [!] Failed to send heartbeat: {response.status} - {text}")
     except Exception as e:
-        print(f"[X] Exception sending heartbeat: {e}")
+        print(f"[{datetime.now().isoformat()}] [X] Exception sending heartbeat: {e}")
 
 
 async def run_heartbeat_loop():
@@ -33,24 +33,11 @@ async def run_heartbeat_loop():
             await asyncio.sleep(INTERVAL_SECONDS)
 
 
-async def run_upload_loop():
-    while True:
-        print("[~] Uploading checkpoint folder...")
-        upload_directory_to_s3(UPLOAD_FOLDER)
-        await asyncio.sleep(UPLOAD_INTERVAL_SECONDS)
-
-
 async def main():
-    await asyncio.gather(
-        run_heartbeat_loop(),
-        # run_upload_loop(),
-    )
+    await asyncio.gather(run_heartbeat_loop())
 
 
 if __name__ == "__main__":
-    print(
-        f"[~] Starting heartbeat + S3 upload loop for {task_id} every {INTERVAL_SECONDS}s"
-    )
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
